43333b6237411d23d84f6c60246c6b0e08e7fec6
not telling <36284560+CrossFIRE121@users.noreply.github.com>
2020-07-26 00:03:52 -0400

    Update index2.js

--- index2.js
+++ index2.js
@@ -17,16 +17,16 @@ fs.readdir("./events/", (err, files) => {
 
 client.commands = new Enmap();
 
-fs.readdir("./commands/", (err, files) => {
-  if (err) return console.error(err);
+fs.readdir("./commands/", (err, files) => { //looks for the command specified by the arguments, eg. ays!ping would have this look for ping.js
+  if (err) return console.error(err); //if theres no file with that name, then abort 
   files.forEach(file => {
-    if (!file.endsWith(".js")) return;
+    if (!file.endsWith(".js")) return; //if the file is named ping.exe, not ping.js, abort
     let props = require(`./commands/${file}`);
     let commandName = file.split(".")[0];
     console.log(`Attempting to load command ${commandName}`);
     client.commands.set(commandName, props);
   });
-
+  //END COMMAND HANDLER//
 })
 
 client.login(config.token);
